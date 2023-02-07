dcfe9483cc819f3e871dbc9db13a51ccb7d223c6
viktoria <bika0724@gmail.com>
2023-02-07 18:03:46 +0300

    firebase : auth&register

--- src/components/pages/auth/Auth.tsx
+++ src/components/pages/auth/Auth.tsx
@@ -1,6 +1,7 @@
 import {FC, SyntheticEvent, useState} from "react";
-import {Button, ButtonGroup, Grid, TextField} from "@mui/material";
+import {Alert, Button, ButtonGroup, Grid, TextField} from "@mui/material";
 import {IUserData} from "./types";
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 
 export const Auth:FC = () => {
     const [isRegForm, setIsRegForm] =useState(false)
@@ -9,14 +10,35 @@ export const Auth:FC = () => {
         password: '',
     } as IUserData);
 
+    const [error, setError] = useState('')
 
 
-    const handleLogin = (e: SyntheticEvent<HTMLFormElement>) => {
+    const handleLogin = async (e: SyntheticEvent<HTMLFormElement>) => {
         e.preventDefault()
+
+        const auth =getAuth()
+
+        //register
         if (isRegForm){
-            console.log('register')
-        } else {
-            console.log('auth')
+            try {
+            await createUserWithEmailAndPassword(
+                auth,
+                userData.email,
+                userData.password)
+            }
+            catch (error:any) {
+                error.message && setError(error.message)
+            }
+        } else { //auth
+            try {
+                await signInWithEmailAndPassword(
+                    auth,
+                    userData.email,
+                    userData.password)
+            }
+            catch (error:any) {
+                error.message && setError(error.message)
+            }
         }
         console.log(userData.email, userData.password)
 
@@ -27,38 +49,42 @@ export const Auth:FC = () => {
     }
 
     return (
-        <Grid display='flex' justifyContent='center' alignItems='center'>
-            <form onSubmit={handleLogin}>
-                <TextField
-                    type='email'
-                    label="Email"
-                    variant="outlined"
-                    value={userData.email}
-                    sx={{
-                        display: 'block',
-                        marginBottom: 3,
-                    }}
-                    onChange={e => setUserData({...userData, email: e.target.value})}
-                    required
-                />
-                <TextField
-                    type='password'
-                    label="Password"
-                    variant="outlined"
-                    value={userData.password}
-                    sx={{
-                        display: 'block',
-                        marginBottom: 3,
-                    }}
-                    onChange={e => setUserData({...userData, password: e.target.value})}
-                    required
-                />
-                <ButtonGroup variant="outlined">
-                    <Button type='submit' onClick={() => setIsRegForm(false)}>Auth</Button>
-                    <Button type='submit' onClick={() => setIsRegForm(true)}>Register</Button>
-                </ButtonGroup>
-            </form>
-        </Grid>
-
+        <>
+            {error && <Alert severity="error" style={{marginBottom:20}}>
+                {error}
+            </Alert>}
+            <Grid display='flex' justifyContent='center' alignItems='center'>
+                <form onSubmit={handleLogin}>
+                    <TextField
+                        type='email'
+                        label="Email"
+                        variant="outlined"
+                        value={userData.email}
+                        sx={{
+                            display: 'block',
+                            marginBottom: 3,
+                        }}
+                        onChange={e => setUserData({...userData, email: e.target.value})}
+                        required
+                    />
+                    <TextField
+                        type='password'
+                        label="Password"
+                        variant="outlined"
+                        value={userData.password}
+                        sx={{
+                            display: 'block',
+                            marginBottom: 3,
+                        }}
+                        onChange={e => setUserData({...userData, password: e.target.value})}
+                        required
+                    />
+                    <ButtonGroup variant="outlined">
+                        <Button type='submit' onClick={() => setIsRegForm(false)}>Auth</Button>
+                        <Button type='submit' onClick={() => setIsRegForm(true)}>Register</Button>
+                    </ButtonGroup>
+                </form>
+            </Grid>
+        </>
     )
 }
---
+++ src/components/providers/AuthProvider.tsx
@@ -0,0 +1,41 @@
+import {createContext, FC, useEffect, useMemo, useState} from "react";
+import {IUser, TypeSetState} from "../addPost/types";
+import {Auth, getAuth, onAuthStateChanged} from 'firebase/auth'
+import {users} from "../layout/sidebar/UserItems/dataUsers";
+
+interface IContext {
+    user: IUser,
+    setUser: TypeSetState<IUser | null>,
+    ga: Auth,
+}
+
+export const AuthContext = createContext<IContext>({} as IContext)
+
+export const AuthProvider:FC<any> = ({children}) => {
+    // @ts-ignore
+    const [user, setUser] = useState<IUser | null>(null)
+
+    const ga = getAuth()
+
+    useEffect(() => {
+        const unListen = onAuthStateChanged(ga, authUser => {
+            setUser(authUser ? {
+             _id: authUser.uid,
+             avatar: users[1].avatar,
+                name: authUser?.displayName,
+            } : null)
+        })
+        return () => {
+            unListen()
+        }
+    }, [])
+
+    const values =useMemo(() => ({
+        user, setUser, ga
+    }), [user,ga])
+
+    return (
+    <AuthContext.Provider value={values}>
+        {children}
+    </AuthContext.Provider>)
+}
